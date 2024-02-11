7b62ce4e057f824178609c40c54e61529a918ccb
TenzDelek <tibetdelek@gmail.com>
2024-02-11 12:40:39 +0530

    middleware

--- 00_Mega/src/app.js
+++ 00_Mega/src/app.js
@@ -2,5 +2,21 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 const app=express()
+//.use is used for middleware
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
 
+//data will come from different ways that we define here
+//through json
+app.use(express.json({limit:"16kb"}))
+
+//through url
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+
+//for image assets
+app.use(express.static("public"))
+
+app.use(cookieParser())
 export{app}
