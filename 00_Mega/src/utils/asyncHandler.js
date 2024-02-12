97e31b209bad95c6166b78909bd4d033d384ca76
TenzDelek <tibetdelek@gmail.com>
2024-02-12 12:16:30 +0530

    created utils

---
+++ 00_Mega/src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+//we are making a wrapper
+
+
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(
+            requestHandler(req,res,next)
+        ).catch((err)=>next(err))
+    }
+}
+
+export {asyncHandler}
+
+//or
+//higher order func
+//see it as 
+//const asyncHandler=(fn)=>{(req,res,next)=>{ }}
+
+// const asyncHandler=(fn)=> async(req,res,next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
