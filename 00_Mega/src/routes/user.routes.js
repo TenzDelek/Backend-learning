53fec2a76814354894c494a862103c9e739865e2
TenzDelek <tibetdelek@gmail.com>
2024-02-29 22:52:02 +0530

    declare the route for the endpoints

--- 00_Mega/src/routes/user.routes.js
+++ 00_Mega/src/routes/user.routes.js
@@ -1,35 +1,64 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser ,refreshAccessToken} from "../controllers/user.controller.js";
-import {upload} from '../middlewares/multer.middleware.js'
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-const router=Router()
+const router = Router();
 
-router.route('/register').post(
-    //middlware to use file
-    upload.fields([
-        {
-            name:"avatar",
-            maxCount:1
-        },
-        {
-            name:"coverImage",
-            maxCount:1
-        }
-    ]),
+router.route("/register").post(
+  //middlware to use file
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
 
-    registerUser
-    )
+  registerUser
+);
 // now the url will look like api/v1/user/register #the api/v1 is consider as standard to write such
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 //this says when route is /login the method that should run is the loginuser one
 
-
 //secure route
-router.route("/logout").post(verifyJWT, logoutUser) //HERE THE VERFITJWT 
+router.route("/logout").post(verifyJWT, logoutUser); //HERE THE VERFITJWT
 //IS THE MIDDLWARE, FIRST IT DOES IT THEN LOGOUT IS DONE AS IN THE VERFIYJWT MIDDLEWARE
 //REMEMBER WE WROTE NEXT(). YA THAT DOES IT LIKE SEND IT TO NEXT
 
+router.route("/refresh-token").post(refreshAccessToken);
 
-router.route("/refresh-token").post(refreshAccessToken)
-export default router
+//verifyjwt is to check first whether the user is login
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails); // patch as we are updating
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+
+//for params
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
+export default router;
